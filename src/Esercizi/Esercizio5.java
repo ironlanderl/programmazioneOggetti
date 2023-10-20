25e101df898008f5543bc246b4872cd89c6b8000
ironlanderl <fabrizio03p@gmail.com>
2023-10-20 11:12:35 +0200

    asfd

--- src/Esercizi/Esercizio5.java
+++ src/Esercizi/Esercizio5.java
@@ -1,6 +1,21 @@
 package Esercizi;
 /*
-* Es5
+* Es7 del file
 * */
+
+import java.util.Scanner;
 public class Esercizio5 {
+    public static void main(String[] args) {
+        System.out.println("Stringa: ");
+        Scanner scan = new Scanner(System.in);
+        String stringa = scan.nextLine();
+        scan.close();
+
+        String[] sottoStringhe = stringa.split(" ");
+
+        for (String a :
+                sottoStringhe) {
+            System.out.println(a);
+        }
+    }
 }
