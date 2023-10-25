50e6c81bed8196869763f2a99a05d49a3e6e076e
ironlanderl <fabrizio03p@gmail.com>
2023-10-25 12:50:35 +0200

    kytf

--- src/Esercizi/Esercizio6.java
+++ src/Esercizi/Esercizio6.java
@@ -6,8 +6,29 @@ package Esercizi;
 import java.util.Scanner;
 
 public class Esercizio6 {
-    Scanner scan = new Scanner(System.in);
-    String stringa = "";
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String stringa = "";
+
+        while (stringa.length() < 2){
+            System.out.println("Stringa min 2 chars:");
+            stringa = scan.nextLine();
+        }
+
+        stringa = scambiaCaratteri(stringa);
+        System.out.printf(stringa);
+
+
+    }
+
+    public static String scambiaCaratteri(String vecchia){
+        Character primo = vecchia.charAt(0);
+        Character ultimo = vecchia.charAt(vecchia.length() - 1);
+
+        String nuova = ultimo + vecchia.substring(1, vecchia.length() - 1) + primo;
+        return nuova;
+    }
 
 
 }
---
+++ src/Esercizi/Esercizio7.java
@@ -0,0 +1,14 @@
+/*
+* Esercizio XIII del file
+*
+*
+* */
+
+
+package Esercizi;
+
+public class Esercizio7 {
+    public static void main(String[] args) {
+        String tmp = "asfsgregsg";
+    }
+}
