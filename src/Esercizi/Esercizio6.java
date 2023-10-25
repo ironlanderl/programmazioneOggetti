e250cd45794b893be79c2e006aaff381b3bd537e
ironlanderl <fabrizio03p@gmail.com>
2023-10-25 11:39:21 +0200

    asd

---
+++ src/Esercizi/Esercizio6.java
@@ -0,0 +1,13 @@
+/*
+* Esercizio XI del file
+* */
+
+package Esercizi;
+import java.util.Scanner;
+
+public class Esercizio6 {
+    Scanner scan = new Scanner(System.in);
+    String stringa = "";
+
+
+}
